b732ffd357a744cfb3aa6d34f36d2a1154ca086b
mark l chaves <mark@marklchaves.com>
2020-10-24 18:02:45 +0800

    Initial check-in

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# cme-user-id-column
- Create custom column to display a sortable user ID column on the Users page.
+# caught my eye Sortable User ID Column Plugin
+
+This WordPres plugin creates custom column to display a sortable user ID column on the Users page.
+
+---
+
+## Installation
+
+1. Manually upload the **contents** of the [zip](cme-user-id-column.zip) file of this repo to /wp-content/plugins/ directory or upload the zip via your wp-admin **Plugins** > **Add New** > **Upload Plugin**.
+2. Activate the plugin through the wp-admin **Plugins** page.
+
+That's it!
+
+---
+
+## Usage
+
+1. Log in to your WordPress /wp-admin/ area.
+2. Go to the Users page.
+3. View User ID column.
+4. Click on the User ID column heading to sort the rows.
+
+---
+
+## Screengrab
+
+![cme-user-id-column-screengrab-1.png](cme-user-id-column-screengrab-1.png)
---
+++ cme-user-id-column.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Plugin Name: caught my eye Sortable User ID Column
+ * Plugin URI: https://github.com/marklchaves/cme-user-id-column/
+ * Description: Create a custom column to display a sortable user ID column on the Users page.
+ * Author URI: https://www.caughtmyeye.cc/
+ * Version: 1.0.0
+ * License: GPL2+
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.txt
+ *
+ * @package CME_USER_ID_COLUMN
+ */
+
+// Exit if accessed directly.
+if (!defined('ABSPATH')) {
+	exit;
+}
+
+/**
+ * Credits
+ * 
+ * Based on code from Piippin's Plugins blog and
+ * Stephen Harris tutorial on Envato Tuts.
+ */
+
+ /**
+ * Display the internal unique user ID
+ * on the wp-admin Users page.
+ */
+
+// Add the User ID Column
+function caughtmyeye_add_user_id_column($columns) {
+    $columns['user_id'] = 'User ID';
+    return $columns;
+}
+add_filter('manage_users_columns', 'caughtmyeye_add_user_id_column');
+ 
+// Display the User ID Column
+function caughtmyeye_show_user_id_column_content($value, $column_name, $user_id) {
+    $user = get_userdata( $user_id );
+	if ( 'user_id' == $column_name )
+		return $user_id;
+    return $value;
+}
+add_action('manage_users_custom_column',  'caughtmyeye_show_user_id_column_content', 10, 3);
+
+// Support Sorting
+function caughtmyeye_sortable_users_column( $columns ) {
+    $columns['user_id'] = 'User ID';
+ 
+    //To make a column 'un-sortable' remove it from the array
+    //unset($columns['date']);
+ 
+    return $columns;
+}
+add_filter( 'manage_users_sortable_columns', 'caughtmyeye_sortable_users_column' );
