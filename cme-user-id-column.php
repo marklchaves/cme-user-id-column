e33554fe9ee3d66fde28caaddf3723440b27be37
mark l chaves <mark@caughtmyeye.cc>
2020-11-07 07:47:35 +0800

    Consistent function naming convention

--- cme-user-id-column.php
+++ cme-user-id-column.php
@@ -2,9 +2,9 @@
 /**
  * Plugin Name: caught my eye Sortable User ID Column
  * Plugin URI: https://github.com/marklchaves/cme-user-id-column/
- * Description: Create a custom column to display a sortable user ID column on the Users page.
+ * Description: Create a custom sortable user ID column on the Users page.
  * Author URI: https://www.caughtmyeye.cc/
- * Version: 1.0.0
+ * Version: 1.1.0
  * License: GPL2+
  * License URI: https://www.gnu.org/licenses/gpl-2.0.txt
  *
@@ -45,12 +45,12 @@ function caughtmyeye_show_user_id_column_content($value, $column_name, $user_id)
 add_action('manage_users_custom_column',  'caughtmyeye_show_user_id_column_content', 10, 3);
 
 // Support Sorting
-function caughtmyeye_sortable_users_column( $columns ) {
+function caughtmyeye_sortable_user_id_column( $columns ) {
     $columns['user_id'] = 'User ID';
  
-    //To make a column 'un-sortable' remove it from the array
-    //unset($columns['date']);
+    // To make a column 'un-sortable' remove it from the array
+    // e.g., unset($columns['date']);
  
     return $columns;
 }
-add_filter( 'manage_users_sortable_columns', 'caughtmyeye_sortable_users_column' );
+add_filter( 'manage_users_sortable_columns', 'caughtmyeye_sortable_user_id_column' );
